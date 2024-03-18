66e2b7f1af4d5508b12b00abf4e087579aa9df9d
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-18 17:56:48 +0000

    files Added

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ function addTask() {
         li.innerHTML = inputBox.value; 
         listContainer.appendChild(li);
         inputBox.value=''
+
+        let span=document.createElement("span");
+        span.innerHTML= "\u00d7"
+
+        li.appendChild(span)
+
         
     }
+    saveData();
 }
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
